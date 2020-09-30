911f38218dacd43931b888bbdc4e1c989d9ff5ba
Adam Sawoscianik <adam.sawoscianik@gmail.com>
2020-09-30 07:44:41 +0200

    added funtions and option to rerun the program

--- main.cpp
+++ main.cpp
@@ -2,18 +2,49 @@
 #include <iostream>
 using namespace std;
 
-int main()  {
-
-    string stringToBeReversed;
-    cout << "Put the string that is to be reversed:" <<endl;
-    getline (cin,stringToBeReversed);
+string reverseAString (string stringToBeReversed) {
     string stringReversed = stringToBeReversed;
     for (uint i=0; i < stringToBeReversed.length(); i++) {
 
         uint reversedIndex = stringToBeReversed.length() - i -1;
         stringReversed[i] = stringToBeReversed[reversedIndex];
     }
-    cout << "This is reversed string: " << stringReversed << endl;
-    return  0;
+    return stringReversed;
+}
 
+string readAString () {
+    string stringToBeReversed;
+    cout << "Put the string that is to be reversed:" <<endl;
+    cin >> stringToBeReversed;
+    //getline (cin,stringToBeReversed);
+    return stringToBeReversed;
+}
+
+void printAString (string stringToBePrinted) {
+
+    cout << "The reversed string is: " << stringToBePrinted << endl;
+}
+
+
+int main()  {
+
+    string stringToBeReversed;
+    while (true) {
+        stringToBeReversed = readAString();
+        printAString(reverseAString(stringToBeReversed));
+
+        char decision = 'A';
+        cout << "Do you want to reverse another string? Y/N: ";
+        cin >> decision;
+
+        if (decision == 'Y') {
+            continue;
+        } else if (decision == 'N') {
+            break;
+        } else {
+            break;
+        }
+
+    }
+    return  0;
 }
